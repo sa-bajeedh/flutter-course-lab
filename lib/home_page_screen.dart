217476641aa7aad58a99bdbcc5c94315f69fb885
sa-bajeedh <saeedmbaj21@gmail.com>
2025-11-30 10:31:27 +0300

    add 2 text field in home page

--- lib/home_page_screen.dart
+++ lib/home_page_screen.dart
@@ -8,10 +8,62 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
+  final _controller1 = TextEditingController();
+  final _controller2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Icon(Icons.home,color: Colors.black,size: 100,),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            SizedBox(
+              height: 100,
+              width: 300,
+              child: TextField(
+                controller: _controller1,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  prefix: Icon(Icons.person, color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 100,
+              width: 300,
+              child: TextField(
+                controller: _controller2,
+                decoration: InputDecoration(
+                  prefix: Icon(Icons.lock, color: Colors.black),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 65,
+              width: 200,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  _controller2.text = _controller1.text;
+                },
+                label: Text(
+                  "طباعة",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+                icon: Icon(Icons.print, color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
